3bedb6b5a4a07604817a29164ea6b20cbc89ba47
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2017-12-24 15:13:58 -0500

    Update fontpixels.js

--- fontpixels.js
+++ fontpixels.js
@@ -1,3 +1,4 @@
+// FontPixels.js by TheGreatRambler https://github.com/TheGreatRambler/fontpixels.js/
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory);
